1b900692ce7580e24c8878e6eaa971db4429f6ad
Anthony <cozziad@gmail.com>
2019-07-10 20:11:15 -0400

    Lesson-098 Done v2

--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import MapKit
 
 class MapVC: UIViewController {
 
+    //outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
     }
 
+    @IBAction func centerMapBtnPressed(_ sender: Any) {
+    }
+    
+}
 
+extension MapVC: MKMapViewDelegate{
+    
 }
 
